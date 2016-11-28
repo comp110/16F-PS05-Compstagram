a78ab9442cfa02106e82ae5ff36d0ec22f643dbb
Kris Jordan <krisjordan@gmail.com>
2016-11-28 13:49:14 -0500

    Final commit before release, I hope

--- src/comp110/filters/BrightnessFilter.java
+++ src/comp110/filters/BrightnessFilter.java
@@ -2,6 +2,18 @@ package comp110.filters;
 
 import comp110.Image;
 
+/**
+ * Author:
+ *
+ * ONYEN:
+ *
+ * Collaborator(s):
+ *
+ * UNC Honor Pledge: I certify that no unauthorized assistance has been received
+ * or given in the completion of this work. I certify that I understand and
+ * could now rewrite on my own, without assistance from collaborators or course
+ * staff, the problem set code I am submitting.
+ */
 public class BrightnessFilter implements Filter {
 
 	public Image process(Image input) {
